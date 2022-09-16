4651a961ae4e668b6a9b538ee7eb69e601c4b65f
unknown <hariharanbemech96@gmail.com>
2022-09-16 23:30:27 +0530

    added how to find first and last element in a sorted array

--- binary_search.js
+++ binary_search.js
@@ -37,3 +37,46 @@ function binarySearchUsingRecursion(arr, left, right, target){
 
 let arr = [10, 20, 30, 50, 60, 80, 110, 130, 140, 170];
 console.log(binarySearchUsingRecursion(arr, 0, arr.length - 1, 110));
+
+/* First and Last position of an element in a sorted array */
+
+var searchRange = function(nums, target) {
+    let low = 0, high = nums.length - 1, res = -1;
+
+    return [firstElem(nums, low, high, target, res), lastElem(nums, low, high, target, res)];
+
+    function firstElem(nums, low, high, target, res){
+        while(low <= high){
+            let mid = low + Math.floor((high - low)/2);
+
+            if(nums[mid] < target) low = mid + 1;
+            else if(nums[mid] > target) high = mid - 1;
+
+            else if(nums[mid] == target){
+                res = mid;
+                high = mid - 1;
+            }
+        }
+        return res;
+    }
+
+    function lastElem(nums, low, high, target, res){
+        while(low <= high){
+            let mid = low + Math.floor((high - low)/2);
+
+            if(nums[mid] < target) low = mid + 1;
+            else if(nums[mid] > target) high = mid - 1;
+
+            else if(nums[mid] == target){
+                res = mid;
+                low = mid + 1;
+            }
+        }
+        return res;
+    }
+
+};
+
+let nums = [0,0,1,2,2], target = 2;
+
+console.log(searchRange(nums, target));
