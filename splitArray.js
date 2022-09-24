0ffe2bf9ebf36a44e5f3d15f53fefe9a1a5ca308
unknown <hariharanbemech96@gmail.com>
2022-09-24 08:52:55 +0530

    splitArray.js

--- splitArray.js
+++ splitArray.js
@@ -1,61 +1,51 @@
-/* Need to Practice */
+function verify(mid, array, n, k){
+    let sum = 0;
+    let count = 0;
 
-function verify(mid, array, n, K)
-{
-    var count = 0;
-    var sum = 0;
-    for (var i = 0; i < n; i++) {
-
-     
-        if (array[i] > mid)
+    for(let i = 0; i < n; i++){
+        if(array[i] > mid){
             return false;
+        }
 
-       
         sum += array[i];
 
-        if (sum > mid) {
+        if(sum > mid){
             count++;
             sum = array[i];
         }
     }
     count++;
 
-   
-    if (count <= K)
-        return true;
+    if(count <= k) return true;
+
     return false;
 }
 
+const splitArray = (array, n, k) => {
+    let max = array.reduce((a,b) => Math.max(a,b));
+    let start = max;
+    let end = 0;
 
-function splitArray(array, n, K)
-{
-    var max = array.reduce((a,b)=>Math.max(a,b));
-    var start = max;
-    var end = 0;
-
-    for (var i = 0; i < n; i++) {
+    for(let i = 0; i < n; i++){
         end += array[i];
     }
 
-   
-    var answer = 0;
-    while (start <= end) {
-        var mid = parseInt((start + end) / 2);
+    let answer = 0;
+    while(start <= end){
+        let mid = start + Math.floor((end - start)/2);
 
-      
-        if (verify(mid, array, n, K)) {
+        if(verify(mid, array, n, k)){
             answer = mid;
             end = mid - 1;
         }
-        else {
+        else{
             start = mid + 1;
         }
     }
-
     return answer;
-}
+};
 
 var array = [1, 3, 2, 4, 10, 8, 4, 2, 5, 3];
 var n = array.length;
-var K = 4;
-console.log( splitArray(array, n, K));
+var k = 4;
+console.log( splitArray(array, n, k));
